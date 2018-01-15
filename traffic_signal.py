c0e8c4d5aac6101a5270db7d58f0de07e0b6010b
Guobi Wu <guobi.wu@gmail.com>
2018-01-15 22:28:03 +0800

    update

--- traffic_signal.py
+++ traffic_signal.py
@@ -2,30 +2,76 @@
 import RPi.GPIO as GPIO
 import time
 
-LedPin = 11    # pin11
+LedPinRed = 11
+LedPinYellow = 13
+LedPinGreen = 15
+
+GreenTime = 10
+YellowTime = 3
+RedTime = 10
+
+def getColorFromPin(pin):
+	color = ""
+	if (pin == LedPinGreen):
+		color = "Green"
+	elif (pin == LedPinYellow):
+		color = "Yellow"
+	elif (pin == LedPinRed):
+		color = "Red"
+	return color
+
+def signalOn(pin, numberOfSeconds):
+	color = getColorFromPin(pin)
+	print('Light {0} for {1} seconds'.format(color, numberOfSeconds))
+	GPIO.output(pin, GPIO.LOW)
+	time.sleep(numberOfSeconds)
+	GPIO.output(pin, GPIO.HIGH)
+	return
+
+def flashLight(pin, times):
+	color = getColorFromPin(pin)
+	print('Flash {0} for {1} times'.format(color, times))
+	for x in range(1, times+1):
+		time.sleep(0.5)
+		print(x)
+		GPIO.output(pin, GPIO.LOW)
+		time.sleep(0.5)
+		GPIO.output(pin, GPIO.HIGH)
 
 def setup():
-	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
-	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+	# Numbers GPIOs by physical location
+	GPIO.setmode(GPIO.BOARD)
+
+	# Set LedPin's mode is output
+	GPIO.setup(LedPinRed, GPIO.OUT)
+	GPIO.setup(LedPinYellow, GPIO.OUT)
+	GPIO.setup(LedPinGreen, GPIO.OUT)
+
+	# Set LedPin high(+3.3V) to off led
+	GPIO.output(LedPinRed, GPIO.HIGH)
+	GPIO.output(LedPinYellow, GPIO.HIGH)
+	GPIO.output(LedPinGreen, GPIO.HIGH)
 
 def loop():
 	while True:
-		print('...led on')
-		GPIO.output(LedPin, GPIO.LOW)  # led on
-		time.sleep(0.5)
-		print('led off...')
-		GPIO.output(LedPin, GPIO.HIGH) # led off
-		time.sleep(0.5)
+		signalOn(LedPinGreen, GreenTime)
+		flashLight(LedPinGreen, 3)
+		flashLight(LedPinYellow, 3)
+		signalOn(LedPinRed, RedTime)
+		flashLight(LedPinRed, 3)
 
 def destroy():
-	GPIO.output(LedPin, GPIO.HIGH)     # led off
-	GPIO.cleanup()                     # Release resource
+	GPIO.output(LedPinRed, GPIO.HIGH)
+	GPIO.output(LedPinYellow, GPIO.HIGH)
+	GPIO.output(LedPinGreen, GPIO.HIGH)
+		
+	GPIO.cleanup()
 
-if __name__ == '__main__':     # Program start from here
+# Program start from here
+if __name__ == '__main__':
 	setup()
 	try:
 		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+	except KeyboardInterrupt:
 		destroy()
-
