c12d809e275f91684858c5cef9569490c9304aa7
Guobi Wu <guobi.wu@gmail.com>
2018-01-22 00:28:47 +0800

    update

--- rotaryLed.py
+++ rotaryLed.py
@@ -6,6 +6,9 @@ RoAPin = 11    # pin11
 RoBPin = 12    # pin12
 RoSPin = 13    # pin13
 
+LedPinRed = 15
+LedPinGreen = 16
+
 globalCounter = 0
 
 flag = 0
@@ -18,6 +21,18 @@ def setup():
 	GPIO.setup(RoBPin, GPIO.IN)
 	GPIO.setup(RoSPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 	rotaryClear()
+
+	global pRed
+	GPIO.setup(LedPinRed, GPIO.OUT)   # Set LedPinRed's mode is output
+	GPIO.output(LedPinRed, GPIO.LOW)  # Set LedPinRed to low(0V)
+	pRed = GPIO.PWM(LedPinRed, 1000)     # set Frequece to 1KHz
+	pRed.start(0)                     # Duty Cycle = 0
+
+	global pGreen
+	GPIO.setup(LedPinGreen, GPIO.OUT)   # Set LedPinRed's mode is output
+	GPIO.output(LedPinGreen, GPIO.LOW)  # Set LedPinRed to low(0V)
+	pGreen = GPIO.PWM(LedPinGreen, 1000)     # set Frequece to 1KHz
+	pGreen.start(0)
 
 def rotaryDeal():
 	global flag
@@ -32,12 +47,13 @@ def rotaryDeal():
 		flag = 0
 		if (Last_RoB_Status == 0) and (Current_RoB_Status == 1):
 			globalCounter = globalCounter + 1
-			# print 'globalCounter = %d' % globalCounter
-			print('globalCounter = {0}'.format(globalCounter))
 		if ((Last_RoB_Status == 1) and (Current_RoB_Status == 0)):
 			globalCounter = globalCounter - 1
-			# print 'globalCounter = %d' % globalCounter
-			print('globalCounter = {0}'.format(globalCounter))
+		if (globalCounter > 100):
+			globalCounter = 100
+		if (globalCounter < 0):
+			globalCounter = 0
+		print('globalCounter = {0}'.format(globalCounter))
 
 def clear(ev=None):
 	global globalCounter
@@ -49,16 +65,20 @@ def clear(ev=None):
 def rotaryClear():
 	GPIO.add_event_detect(RoSPin, GPIO.FALLING, callback=clear) # wait for falling
 
-
 def loop():
 	global globalCounter
 	while True:
 		rotaryDeal()
-#		print 'globalCounter = %d' % globalCounter
+		pRed.ChangeDutyCycle(globalCounter)
+		pGreen.ChangeDutyCycle(100 - globalCounter)
 
 def destroy():
-	GPIO.cleanup()             # Release resource
-
+	pRed.stop()
+	GPIO.output(LedPinRed, GPIO.LOW)    # turn off all leds
+	pGreen.stop()
+	GPIO.output(LedPinRed, GPIO.LOW)    # turn off all leds
+	GPIO.cleanup()
+	
 if __name__ == '__main__':     # Program start from here
 	setup()
 	try:
